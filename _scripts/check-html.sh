0d869e107f65815031792a5afc1673c38d23078f
Travis Downs <travis.downs@gmail.com>
2021-08-14 22:23:33 -0700

    ignore status 429 in html link checker

--- _scripts/check-html.sh
+++ _scripts/check-html.sh
@@ -7,4 +7,6 @@ set -euo pipefail
 
 echo "Checking ${SITE:=_site}"
 
-htmlproofer --assume-extension --url-ignore '/.*/notexist.html/' "$@" "$SITE"
+# we ignore status 429 since we get these fairly frequency from github for our site
+# pages since I guess this pounds the site pretty hard and pages is like "woah, slow down buddy"
+htmlproofer --assume-extension --http-status-ignore=429 --url-ignore '/.*/notexist.html/' "$@" "$SITE"
