f0e193bd840d68738710bb8578e02418b01fcaa8
Travis Downs <travis.downs@gmail.com>
2021-08-31 19:40:09 -0700

    add miscellaneous checks script

---
+++ _scripts/checks/misc.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# Miscellaneous checks
+#
+set -euo pipefail
+
+echo "Running miscellaenous checks on ${SRC:=.}, ${SITE:=_site}"
+
+# https://stackoverflow.com/a/7170782
+# exts=$(find . -type f -printf "%f\n" | grep -F '.' | awk -F. '!a[$NF]++{print $NF}' | sort)
+
+# echo "Line counts by extension:"
+# for ext in $exts; do
+#     printf '%10s:' "$ext"
+#     printf '%8d\n' "$( ( find ./ -name "*.$ext" -print0 | xargs -0 cat ) | wc -l)"
+# done
+
+# this trickery is just to avoid having a literal T-O-D-O in this
+# script since it will trigger the check below!
+word="TOD"O
+
+grep --exclude='*.svg' --exclude-dir='.git' -n -I -i -r $word "$SRC"
+if [[ $? -ne 1 ]]; then
+    echo "$word found in source, see above"
+    exit 1
+fi
