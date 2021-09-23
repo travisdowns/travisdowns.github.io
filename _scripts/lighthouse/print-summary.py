7ae85fd78ea8fe74e70464dbaa512e7e56a02e9f
Travis Downs <travis.downs@gmail.com>
2021-09-23 15:13:46 -0700

    print lighthouse summary

---
+++ _scripts/lighthouse/print-summary.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import sys
+import json
+import re
+
+columns = [ "performance", 
+            "accessibility", 
+            "best-practices",
+            "seo" ]
+
+if len(sys.argv) > 1:
+    with open(sys.argv[1]) as f:
+        input = f.read()
+else:
+    input = sys.stdin.read()
+
+parsed = json.loads(input)
+
+rep = []
+maxlen = 0
+for entry in parsed:
+    if (entry['isRepresentativeRun']):
+        entry['stripped'] = re.sub('^http://localhost:[0-9]*', '', entry['url'])
+        rep.append(entry)
+        maxlen = max(maxlen, len(entry['stripped']))
+
+# print the header
+urlfmt = '{:' + str(maxlen) + '}'
+print(urlfmt.format('url'), end='')
+for c in columns:
+    print('{:>16}'.format(c), end='')
+print()
+
+# print the data
+for entry in rep:
+    print(urlfmt.format(entry['stripped']), end='')
+    summary = entry['summary']
+    for c in columns:
+        print('{:16.0f}'.format(summary[c] * 100), end='')
+    print()
+
