484873ad85ef4e3bceac569847c5a9e11d8798a9
travisdowns <travisdowns@users.noreply.github.com>
2021-10-01 17:16:01 +0000

    Site build for: 42c2cfd956f02c05fe59948ecaa3417e99d7ef27 add mark mode override, settings page and banner

--- docs/assets/dark-mode.js
+++ docs/assets/dark-mode.js
@@ -1,3 +1,5 @@
+'use strict';
+
 (function(i) {
   
   i.setSheet = function (color) {
@@ -13,15 +15,27 @@
   }
 
   /**
-   * See if localStorage apears to be working.
+   * Check if the given storage seems to be working.
+   * @param {*} storage storage object to check
    */
-  i.lsOk = function() {
+  var storageOk = function(storage) {
     try {
-      localStorage.setItem('lsOk', 'x');
-      return localStorage.getItem('lsOk') === 'x';
+      storage.setItem('dm-test', 'x');
+      return storage.getItem('dm-test') === 'x';
     } catch (e) {
       return false;
     }
+  }
+
+  /**
+   * See if localStorage apears to be working.
+   */
+  i.lsOk = function() {
+    return storageOk(localStorage);  
+  }
+
+  i.bothOk = function() {
+    return storageOk(localStorage) && storageOk(sessionStorage);
   }
 
   
@@ -44,7 +58,57 @@
       console.error('bad override: ' + value);
     }
     i.updateSheet();
+  }
   
+  i.closeCount = function() {
+    return parseInt(localStorage.getItem('dm-close-count') || '0');
+  }
+
+  i.showDmBar = function() {
+    // We show the bar if we are in "default" light mode, i.e.,
+    // not overrideden. Additionally, we check that session and
+    // local storage are working, because if not we aren't 
+    // going to be able to hide the bar, so better to now show
+    // it at all. We also show the bar if we are overriden to dark
+    // mode and this override was set via the bar, since we don't 
+    // want the bar to suddenly disapear when the box is checked.
+    // Finally, we don't show the bar if the user has
+    // closed it this session, or twice ever (saved in localStorage)
+    if ('true' === 'true') {
+      return false;
+    }
+    if (!i.bothOk() || sessionStorage.getItem('dm-closed') || i.closeCount() >= 2) {
+      return false;
+    }
+    if (i.get() === 'light') {
+      return !i.getOverride();
+    } else {
+      return localStorage.getItem('bar-used');
+    }
+  }
+
+
+  var dmHeader = document.getElementById('dm-header');
+
+  i.closeBar = function() {
+    dmHeader.classList.add('hidden');
+    sessionStorage.setItem('dm-closed', 'true');
+    localStorage.setItem('dm-close-count', i.closeCount() + 1);
+  }
+
+  if (i.showDmBar()) {
+    dmHeader.classList.remove('hidden');
+    var check = document.getElementById('dm-select');
+    check.addEventListener('change', function(e) {
+      // console.log('box is ' + (this.checked ? 'checked' : 'unchecked'));
+      if (this.checked) {
+        localStorage.setItem('bar-used', 'true');
+        DARKMODE.override('dark');
+      } else {
+        localStorage.removeItem('bar-used', 'true');
+        DARKMODE.override(null);
+      }
+    });
   }
   
 }(DARKMODE));
