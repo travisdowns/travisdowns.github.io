91e4665b39c20684c0d6ddc5933209c64df36791
Travis Downs <travis.downs@gmail.com>
2021-10-04 10:00:23 -0700

    more compact output for lighthouse summary

--- _scripts/lighthouse/print-summary.py
+++ _scripts/lighthouse/print-summary.py
@@ -4,10 +4,10 @@ import sys
 import json
 import re
 
-columns = [ "performance", 
-            "accessibility", 
-            "best-practices",
-            "seo" ]
+columns = [ ('performance', 'perf'),
+            ('accessibility', 'a11y'),
+            ('best-practices', 'best'),
+            ('seo', 'seo') ]
 
 if len(sys.argv) > 1:
     with open(sys.argv[1]) as f:
@@ -21,29 +21,32 @@ rep = []
 maxlen = 0
 for entry in parsed:
     if (entry['isRepresentativeRun']):
-        entry['stripped'] = re.sub('^http://localhost:[0-9]*', '', entry['url'])
+        entry['stripped'] = re.sub('^.*/', '', entry['url'])
         rep.append(entry)
         maxlen = max(maxlen, len(entry['stripped']))
+
+width = 8
 
 # print the header
 urlfmt = '{:' + str(maxlen) + '}'
 print(urlfmt.format('url'), end='')
-for c in columns:
-    print('{:>16}'.format(c), end='')
+for _, name in columns:
+    print('{:>{width}}'.format(name, width=width), end='')
 print()
+
 
 # print the data
 for entry in rep:
-    print(urlfmt.format(entry['stripped']), end='')
+    print(urlfmt.format(entry['stripped'], width=width), end='')
     summary = entry['summary']
-    for c in columns:
-        v = summary[c]
+    for col, _ in columns:
+        v = summary[col]
         if v is not None:
-            print('{:16.0f}'.format(v * 100), end='')
+            print('{:{width}.0f}'.format(v * 100, width=width), end='')
         else:
             # sometimes the score is missing, indicating a failure
             # in the underlying lighthouse testing module
-            print('{:>16}'.format('error'), end='')
+            print('{:>{width}}'.format('error', width=width), end='')
 
     print()
 
