b589e03ce3e132e0b39ae62d91123b1c67a0f85d
Travis Downs <travis.downs@gmail.com>
2021-10-06 23:07:30 -0700

    add shortened result links to LHCI output

--- _scripts/lighthouse/print-summary.py
+++ _scripts/lighthouse/print-summary.py
@@ -3,19 +3,36 @@
 import sys
 import json
 import re
+from os import getenv
+
+if getenv('DEBUG') == '1':
+    d = lambda *a: print(file=sys.stderr, *a)
+else:
+    d = lambda *a: None
+
+# prints a summary of the lighthouse results based on a manifest file and
+# optionally a links file with links to uploaded results
 
 columns = [ ('performance', 'perf'),
             ('accessibility', 'a11y'),
             ('best-practices', 'best'),
-            ('seo', 'seo') ]
+            ('seo', 'seo')
+          ]
 
-if len(sys.argv) > 1:
-    with open(sys.argv[1]) as f:
-        input = f.read()
+with open(sys.argv[1]) as f:
+    manifest = f.read()
+
+if len(sys.argv) > 2:
+    with open(sys.argv[2]) as f:
+        linkstr = f.read()
 else:
-    input = sys.stdin.read()
+    linkstr = '{}'
 
-parsed = json.loads(input)
+parsed = json.loads(manifest)
+d('manifest:\n', json.dumps(parsed, indent=2))
+
+links = json.loads(linkstr)
+d('links:\n' + json.dumps(links, indent=2))
 
 rep = []
 maxlen = 0
@@ -32,8 +49,17 @@ urlfmt = '{:' + str(maxlen) + '}'
 print(urlfmt.format('url'), end='')
 for _, name in columns:
     print('{:>{width}}'.format(name, width=width), end='')
-print()
+print('  link')
 
+pattern = re.compile('^https://storage\\.googleapis\\.com/lighthouse-infrastructure\\.appspot\\.com/reports/(.*)\\.report\\.html$')
+
+def make_link(url):
+    if not url:
+        return 'missing'
+    m = pattern.match(url)
+    if not m:
+        return 'unexpected link format' + url
+    return 'https://travisdowns.github.io/s#l' + m.group(1)
 
 # print the data
 for entry in rep:
@@ -47,6 +73,6 @@ for entry in rep:
             # sometimes the score is missing, indicating a failure
             # in the underlying lighthouse testing module
             print('{:>{width}}'.format('error', width=width), end='')
-
-    print()
+    link = links.get(entry['url'])
+    print('  ', make_link(link), sep='')
 
