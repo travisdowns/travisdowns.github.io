68bf1e609603e93a1df227e01c1357bd29595ebe
Travis Downs <travis.downs@gmail.com>
2022-10-15 16:25:43 -0700

    Let MAX_TRIES be configurable
    ... in the external-links.sh check.

--- _scripts/checks/external-links.sh
+++ _scripts/checks/external-links.sh
@@ -6,4 +6,4 @@ set -euo pipefail
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-MAX_TRIES=3 "$SCRIPT_DIR/call-htmlproofer.sh" --external-only
+MAX_TRIES=${MAX_TRIES-3} "$SCRIPT_DIR/call-htmlproofer.sh" --external-only
